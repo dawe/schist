4f1392d22c2668d154d107623607855f150332a1
Cittaro Davide <cittaro.davide@gmail.com>
2020-02-26 15:45:01 +0100

    Added read/write functions to save NestedBlockState as a separate pickle

--- scnsbm/utils.py
+++ scnsbm/utils.py
@@ -1,8 +1,9 @@
 """Utility functions and classes
 """
 import numpy as np
-#from anndata import AnnData
+import anndata
 from scanpy import logging as logg
+import pickle
 
 def get_graph_tool_from_adjacency(adjacency, directed=None):
     """Get graph-tool graph from adjacency matrix."""
@@ -25,3 +26,49 @@ def get_graph_tool_from_adjacency(adjacency, directed=None):
             'Your adjacency matrix contained redundant nodes.'
         )
     return g
+
+
+## I/O utils, these are useful to read/write objects when the state is there
+
+def save(adata, prefix='adata', key='nsbm', h5ad_fname=None, pkl_fname=None):
+    """Save anndata object when a NestedBlockState has been retained during
+    inference. The `state` object is stripped out the `adata.uns` and saved as pickle
+    separately.
+    """
+    state = None
+    if 'state' in adata.uns[key]:
+        state = adata.uns[key].pop('state')
+    if not h5ad_fname:
+        h5ad_fname = "%s.h5ad" % prefix
+    if not pkl_fname:
+        pkl_fname = "%s.pkl" % prefix
+    # write the anndata
+    adata.write(h5ad_fname)
+    if state:
+        with open(pkl_fname, 'wb') as fh:
+            pickle.dump(state, fh, 2)
+
+def read(adata, prefix='adata', key='nsbm', h5ad_fname=None, pkl_fname=None):
+    """Read anndata object when a NestedBlockState has been saved separately.
+    This function reads the h5ad and the pkl files, then rebuilds the `adata` properly,
+    returning it to the user.
+    """
+    if not h5ad_fname:
+        h5ad_fname = "%s.h5ad" % prefix
+    if not pkl_fname:
+        pkl_fname = "%s.pkl" % prefix
+    
+    # read the anndata
+    adata = anndata.read_h5ad(h5ad_fname)
+    
+    try:
+        with open(pkl_fname, 'rb') as fh:
+            state = pickle.load(fh)
+            adata.uns[key]['state'] = state
+    except IOError:
+        logg.warning(
+            f'The specified file for state {pkl_fname} does not exist. '
+            'Proceeding anyway'
+        )
+        pass            
+    return adata        
