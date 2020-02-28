5a16d325be2b85a2aafe439f3758f425cb9ee3d8
Cittaro Davide <cittaro.davide@gmail.com>
2020-02-28 19:50:12 +0100

    added info

--- scnsbm/__init__.py
+++ scnsbm/__init__.py
@@ -4,4 +4,14 @@ from . import plotting as pl
 from . import io
 
 
+__author__ = ', '.join([
+    'Davide Cittaro',
+    'Leonardo Morelli',
+])
+__email__ = ', '.join([
+    'cittaro.davide@hsr.it',
+    'l.morelli2@studenti.unisr.it',
+])
+
+# one day I will figure out how to make this automagically
 __version__ = '0.2.4'
