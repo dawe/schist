8e2a6e8872cfd6fbcbf227f9b29b42ec6ad8fe03
Cittaro Davide <cittaro.davide@gmail.com>
2020-03-21 10:29:22 +0100

    again travis

--- conftest.sh
+++ conftest.sh
@@ -1,9 +1,9 @@
 #lib/python3.6/site-packages/graph_tool/inference/mcmc.py
 
-python_v=`python -V | cut -f 1,2 -d. | awk '{print $2}'`
-cwd=`cwd`
+#python_v=`python -V | cut -f 1,2 -d. | awk '{print $2}'`
+cwd=`pwd`
 
-pushd $CONDA_PREFIX/lib/python${python_v}/site-packages/graph_tool/inference
+pushd $CONDA_PREFIX/lib/python3.6/site-packages/graph_tool/inference
 patch -p0 < ${cwd}/patch.mcmc.txt
 
 popd
