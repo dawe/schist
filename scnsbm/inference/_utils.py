ca0869a66e4802aecedd68c1811921ab03778483
Cittaro Davide <cittaro.davide@gmail.com>
2020-04-01 18:25:32 +0200

    added a function for virtual moves

---
+++ scnsbm/inference/_utils.py
@@ -0,0 +1,39 @@
+import graph_tool.all as gt
+import numpy as np
+
+def get_cell_loglikelihood(
+    state,
+    level: int = 0,
+    rescale = False
+    
+):
+    """
+    Returns the matrix of log-likelihood differences
+    when moving a cell into a different block
+    """
+    
+    # get the graph from state
+    g = state.g
+    if level < 0 or level > len(state.get_levels()):
+        # by now return the lowest level if invalid 
+        
+        level = 0
+    
+    if type(state) == graph_tool.inference.nested_blockmodel.NestedBlockState:
+        B = gt.BlockState(g, b=state.project_partition(level, 0))
+    else:
+        B = state
+        
+    n_cells = g.num_vertices()
+    n_blocks = B.get_nonempty_B()
+    
+    shape = (n_cells, n_blocks)
+    
+    M = np.array([B.virtual_vertex_move(v, s) for v in range(n_cells) for s in range(n_blocks)]).reshape(shape)
+    
+    if rescale:
+        # some cells may be better in other groups, hence their LL
+        # is negative when moved. Rescaling sets the minimum LL in the
+        # best group
+        M = M - np.min(M, axis=1)[:, None]
+    return M        
