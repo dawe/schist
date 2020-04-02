79ce714f22a75bf64d45d980b670b936019abd7b
Cittaro Davide <cittaro.davide@gmail.com>
2020-04-02 17:26:23 +0200

    conftest

--- conftest.py
+++ conftest.py
@@ -8,3 +8,4 @@ scnsbm.inference.nested_model(adata, fast_model=True, use_weights=False,  wait=1
 scnsbm.io.write(adata, prefix='test')
 adata = scnsbm.io.read(prefix='test')
 scnsbm.inference.flat_model(adata, fast_model=True, use_weights=False, wait=10)          
+scnsbm.inference.flat_model(adata, resume=True, wait=10)
