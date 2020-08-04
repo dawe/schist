7db92ecf66f8939057f22e4b23758035242731da
Valentina Giansanti <valentinagiansanti@Valentinas-MacBook-Air.local>
2020-08-04 13:09:50 +0200

    first docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,7 @@ author = 'Davide Cittaro, Leonardo Morelli'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-	      'sphinx.ext.pngmath'
+extensions = ['sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
