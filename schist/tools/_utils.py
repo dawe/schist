13d7342d309154048d0eab7cb5c828edacd3c26c
Cittaro Davide <cittaro.davide@gmail.com>
2021-04-26 18:11:50 +0200

    added new function to get "move back" probability

--- schist/tools/_utils.py
+++ schist/tools/_utils.py
@@ -74,3 +74,54 @@ def get_cell_loglikelihood(
 
             
     return M
+
+def get_cell_back_p(
+    state: Union[graph_tool.inference.nested_blockmodel.NestedBlockState, graph_tool.inference.planted_partition.PPBlockState],
+    level: int = 0,
+    rescale: bool = False, 
+    as_prob: bool = False,
+    
+):
+    """
+    Returns the matrix of log-likelihood differences
+    when moving a cell into a different block
+    
+    Parameters
+    ----------
+    state
+        A graphtool BlockState or NestedBlockState objexct
+    level
+        The level in NestedBlockState to consider
+    rescale
+        For some models, moving a cell into a different block may result in a 
+        negative log-likelihood, indicating that cells may be better assigned 
+        to another group. Set this parameter to `True` if you want 
+        every cell to have LL=0 for the best group and avoid negative values
+    as_prob
+        Return values as probabilites
+
+    Returns
+    -------
+    `M`
+        Array of dim (n_cells, n_blocks) that stores the entropy difference
+        of moving a cell into a specific group
+    """
+    
+    # get the graph from state
+#    g = state.g
+    
+    try:
+        if level < 0 or level > len(state.get_levels()):
+            # by now return the lowest level if invalid 
+            level = 0
+        B = gt.BlockState(state.g, b=state.project_partition(level, 0))
+    except AttributeError:
+        B = state
+    
+    
+    n_cells = state.g.num_vertices()
+    n_blocks = B.get_nonempty_B()
+    shape = (n_cells, n_blocks)
+    M = np.array([B.get_move_prob(v, s, reverse=True) for v in range(n_cells) for s in range(n_blocks)]).reshape(shape)
+
+    return M    
