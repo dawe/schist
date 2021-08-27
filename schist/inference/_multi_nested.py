41f1dfe37c23c0438b8074656cb62e57884767b5
Cittaro Davide <cittaro.davide@gmail.com>
2021-08-27 17:31:36 +0200

    More on group subsets

--- schist/inference/_multi_nested.py
+++ schist/inference/_multi_nested.py
@@ -324,10 +324,11 @@ def nested_model_multi(
         if collect_marginals:
             # add marginals for level 0, the sum up according to the hierarchy
             adatas[xn].obsm[f"CM_{key_added}_level_0"] = pv_array
+            _groups = groups.loc[adatas[xn].obs_names]
+            _pv_array = pd.DataFrame(pv_array).loc[adatas[xn].obs_names]
             for group in groups.columns[1:]:
-                _groups = groups.loc[adatas[xn].obs_names]
                 ct = pd.crosstab(_groups[_groups.columns[0]], _groups[group], normalize='index')
-                adatas[xn].obsm[f'CM_{group}'] = pv_array @ ct.values
+                adatas[xn].obsm[f'CM_{group}'] = _pv_array @ ct.values
 
         # last step is recording some parameters used in this analysis
         adata.uns['schist']['multi_level_params'] = dict(
