3d5323a75e896c6097e61007e6232e9d21527e8d
Leonardo Morelli <57952505+leomorelli@users.noreply.github.com>
2021-10-20 17:00:03 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,7 +87,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'custom'
+html_theme = 'alabaster'
 
 #html_theme = 'classic'
 #html_theme_options = {
