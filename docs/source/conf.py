a2451574f73a58ebdb1f2565ada1324b591b5cb7
Cittaro Davide <cittaro.davide@gmail.com>
2021-11-29 16:56:11 +0100

    modified theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -111,7 +111,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme' #'sphinxdoc'
 html_theme_options = {
     'logo': 'images/garnet.png',
     'logo_name': 'true',
