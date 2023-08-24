815146e8b76080cb08c36aa5468b5f95cf4a2e0f
Cittaro Davide <cittaro.davide@gmail.com>
2023-08-24 15:24:17 +0200

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -47,7 +47,7 @@ copyright = '2021, Morelli Leonardo, Giansanti Valentina, Cittaro Davide'
 author = 'Morelli Leonardo, Giansanti Valentina, Cittaro Davide'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.1'
+release = '0.8.2'
 
 
 # -- General configuration ---------------------------------------------------
