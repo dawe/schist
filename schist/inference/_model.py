fce4ae3d2dc067715565d1bdec3beec9133cba73
Cittaro Davide <cittaro.davide@gmail.com>
2024-07-17 12:06:21 +0200

    function docs fixes

--- schist/inference/_model.py
+++ schist/inference/_model.py
@@ -40,14 +40,12 @@ def model(
     n_sweep: int = 10,
     beta: float = np.inf,
     n_init: int = 100,
+    model: Literal["nsbm", "sbm", "ppbm"] = "nsbm",
+    max_iter: int = 500,
     collect_marginals: bool = True,
-    n_jobs: int = -1,
     refine_model: bool = False,
     refine_iter: int = 100,
-    max_iter: int = 500,
-    model: Literal["nsbm", "sbm", "ppbm"] = "nsbm",
-    *,
-    random_seed: Optional[int] = None,
+    n_jobs: int = -1,
     key_added: str | None = None,
     adjacency: Optional[sparse.spmatrix] = None,
     neighbors_key: Optional[str] = 'neighbors',
@@ -55,18 +53,13 @@ def model(
     use_weights: bool = False,
     save_model: Union[str, None] = None,
     copy: bool = False,
+    random_seed: Optional[int] = None,
     dispatch_backend: Optional[str] = 'loky',
 ) -> Optional[AnnData]:
     """\
-    Cluster cells into subgroups [Peixoto14]_.
-
     Cluster cells using the nested Stochastic Block Model [Peixoto14]_,
-    a hierarchical version of Stochastic Block Model [Holland83]_, performing
-    Bayesian inference on node groups. NSBM should circumvent classical
-    limitations of SBM in detecting small groups in large graphs
-    replacing the noninformative priors used by a hierarchy of priors
-    and hyperpriors.
-
+    performing Bayesian inference on node groups. 
+    
     This requires having ran :func:`~scanpy.pp.neighbors` or
     :func:`~scanpy.external.pp.bbknn` first.
 
@@ -85,15 +78,21 @@ def model(
     beta
         Inverse temperature for MCMC greedy approach    
     n_init
-        Number of initial minimizations to be performed. The one with smaller
-        entropy is chosen
+        Number of concurrent minimizations to be performed. The final model will be
+        a consensus over these models.
+    model
+        The SBM model to use. `nsbm` implements Nested Stochastic Block Model. 
+        `sbm` is the Stochastic Block Model. `ppbm` is the Planted Partition Block Model
+        which only has an assortativity prior.
+    max_iter
+        Maximum number of iterations during minimization, set to infinite to stop 
+        minimization only on tolerance
+    collect_marginals
+        Collect marginal distribution of blocks    
     refine_model
         Wether to perform a further mcmc step to refine the model
     refine_iter
         Number of refinement iterations.
-    max_iter
-        Maximum number of iterations during minimization, set to infinite to stop 
-        minimization only on tolerance
     n_jobs
         Number of parallel computations used during model initialization
     key_added
@@ -112,7 +111,8 @@ def model(
         increases computation times
     save_model
         If provided, this will be the filename for the PartitionModeState to 
-        be saved    
+        be saved. The PartitionModeState contains all the models minimized during 
+        inference.
     copy
         Whether to copy `adata` or modify it inplace.
     random_seed
@@ -123,15 +123,14 @@ def model(
     `adata.obs[key_added]`
         Array of dim (number of cells) that stores the subgroup id
         (`'0'`, `'1'`, ...) for each cell. 
-    `adata.uns['schist']['params']`
-        A dict with the values for the parameters `resolution`, `random_state`,
-        and `n_iterations`.
-    `adata.uns['schist']['stats']`
-        A dict with the values returned by mcmc_sweep
-    `adata.obsm['CA_nsbm_level_{n}']`
+    `adata.uns['schist'][model]['stats']`
+        A dict with entropy and modularity values
+    `adata.uns['schist'][model]['params']`
+        A dict with the values for the parameters used
+    `adata.obsm['CM_nsbm_level_{n}']` or `adata.obsm['CM_model']`
         A `np.ndarray` with cell probability of belonging to a specific group
-    `adata.uns['schist']['state']`
-        The NestedBlockModel state object
+    `adata.uns['schist'][model]['state']`
+        The block model, to be used in case a gt state should be initialized
     """
 
     gt_model = {'nsbm':gt.NestedBlockState, 
@@ -367,13 +366,13 @@ def model(
 
     if not 'schist' in adata.uns:
         adata.uns['schist'] = {}
-    adata.uns['schist'][f'{model}'] = {}
-    adata.uns['schist'][f'{model}']['stats'] = dict(
+    adata.uns['schist'][model] = {}
+    adata.uns['schist'][model]['stats'] = dict(
     entropy=state.entropy(),
     modularity=modularity
     )
     if model == "nsbm":
-        adata.uns['schist'][f'{model}']['stats']['level_entropy']=np.array([state.level_entropy(x) for x in range(len(state.levels))])
+        adata.uns['schist'][fmodel]['stats']['level_entropy']=np.array([state.level_entropy(x) for x in range(len(state.levels))])
 
     if model == "nsbm":
         # record state as list of blocks
@@ -385,10 +384,10 @@ def model(
     else:
         bl_d = {'0':np.array(state.get_blocks().a)}        
     
-    adata.uns['schist'][f'{model}']['blocks'] = bl_d
+    adata.uns['schist'][model]['blocks'] = bl_d
 
     # last step is recording some parameters used in this analysis
-    adata.uns['schist'][f'{model}']['params'] = dict(
+    adata.uns['schist'][model]['params'] = dict(
         model=model,
         neighbors_key=neighbors_key,
         use_weights=use_weights,
