eedb354403c109746af4368a428fc81369eb9ef6
Cittaro Davide <cittaro.davide@gmail.com>
2024-07-17 15:38:03 +0200

    removed support for leiden

--- schist/inference/__init__.py
+++ schist/inference/__init__.py
@@ -3,5 +3,5 @@ from ._model_multi import model_multi
 from ._nested_model import nested_model
 from ._flat_model import flat_model
 from ._planted_model import planted_model
-from ._pmleiden import leiden
+#from ._pmleiden import leiden
 from ._multi_nested import nested_model_multi
