ce9cc6ae9554578968e4f32140cdb195216dc550
Cittaro Davide <cittaro.davide@gmail.com>
2024-07-18 17:08:05 +0200

    start supporting MuData

--- schist/inference/_model_multi.py
+++ schist/inference/_model_multi.py
@@ -3,6 +3,7 @@ from typing import Optional, Tuple, Sequence, Type, Union, Dict, List, Literal
 import numpy as np
 import pandas as pd
 from anndata import AnnData
+from mudata import MuData
 from scipy import sparse
 from natsort import natsorted
 from joblib import delayed, Parallel, parallel_config
