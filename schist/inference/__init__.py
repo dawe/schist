c3752eda6cef27bc0cde04f08b93a2014c90fa72
Cittaro Davide <cittaro.davide@gmail.com>
2024-07-18 09:40:23 +0200

    fixed version deps

--- schist/inference/__init__.py
+++ schist/inference/__init__.py
@@ -3,5 +3,4 @@ from ._model_multi import model_multi
 from ._nested_model import nested_model
 from ._flat_model import flat_model
 from ._planted_model import planted_model
-#from ._pmleiden import leiden
 from ._multi_nested import nested_model_multi
