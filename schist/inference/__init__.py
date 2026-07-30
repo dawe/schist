7d1a632f66cad3bd5f349a5411fcca4573a10250
dawe <daweonline@gmail.com>
2026-07-30 09:35:08 +0200

    removed support for legacy function names

--- schist/inference/__init__.py
+++ schist/inference/__init__.py
@@ -1,6 +1,2 @@
 from ._model import fit_model
 from ._model_multi import fit_model_multi
-from ._nested_model import nested_model
-from ._flat_model import flat_model
-from ._planted_model import planted_model
-from ._multi_nested import nested_model_multi
