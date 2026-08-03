ad3691371ca906a0114433cec9ea8df1b9718a6c
dawe <daweonline@gmail.com>
2026-08-03 14:18:35 +0200

    test if pclabel works

--- schist/inference/_model.py
+++ schist/inference/_model.py
@@ -233,14 +233,14 @@ def fit_model(
             raise AttributeError(f"{constraint_key} must be categorical")
         pclabel = g.new_vp('int64_t')
         pclabel.a = np.array(adata.obs[constraint_key].cat.codes)
-        if nested:
-            raise NotImplementedError("Constraints do not (yet) work with NSBM")
-#            base_state_args['pclabel'] = pclabel
-#            base_state_args['clabel'] = pclabel
+#        if nested:
+#            raise NotImplementedError("Constraints do not (yet) work with NSBM")
+            base_state_args['pclabel'] = pclabel
+            base_state_args['clabel'] = pclabel
         elif assortative:
-             raise NotImplementedError("Constraints do not (yet) work with PPBM")
-#            base_state_args['pclabel'] = pclabel
-#            base_state_args['clabel'] = pclabel
+#             raise NotImplementedError("Constraints do not (yet) work with PPBM")
+            base_state_args['pclabel'] = pclabel
+            base_state_args['clabel'] = pclabel
         else:
             state_args['pclabel'] = pclabel
             state_args['clabel'] = pclabel
